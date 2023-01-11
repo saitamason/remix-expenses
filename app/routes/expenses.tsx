9639de296ef04a4f343b98a50b9dfe7797609ac3
dawidlehai <haidawidle@gmail.com>
2023-01-11 14:05:38 +0100

    Prepared analytics and layout for next steps

--- app/routes/expenses.tsx
+++ app/routes/expenses.tsx
@@ -1,12 +1,31 @@
 import type { LinksFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import ExpensesList from "~/components/expenses/ExpensesList";
 import expensesStyles from "~/styles/expenses.css";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second expense",
+    amount: 19.99,
+    date: new Date().toISOString(),
+  },
+];
 
 export default function ExpensesLayout() {
   return (
-    <main>
+    <>
       <Outlet />
-    </main>
+      <main>
+        <ExpensesList expenses={DUMMY_EXPENSES} />
+      </main>
+    </>
   );
 }
 
