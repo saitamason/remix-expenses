91f521c477df2d2e9aa9fa18b7089ab2a2adc130
dawidlehai <haidawidle@gmail.com>
2023-01-11 14:47:17 +0100

    Modals can be closed when the background is clicked

--- app/routes/expenses/$id.tsx
+++ app/routes/expenses/$id.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function ExpenseDetailsPage() {
+  const navigate = useNavigate();
+  const closeHandler = () => navigate("..");
+
   return (
-    <Modal onClose={() => {}}>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
