86483271f676b4cf71c92c3c7781e58e163e3709
dawidlehai <haidawidle@gmail.com>
2023-01-11 13:48:01 +0100

    Basic analytics with dummy data

--- app/routes/expenses/analytics.tsx
+++ app/routes/expenses/analytics.tsx
@@ -1,3 +1,26 @@
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import Chart from "~/components/expenses/Chart";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second expense",
+    amount: 19.99,
+    date: new Date().toISOString(),
+  },
+];
+
 export default function ExpensesAnalyticsPage() {
-  return <p>Expense analytics</p>;
+  return (
+    <main>
+      <Chart expenses={DUMMY_EXPENSES} />
+      <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+    </main>
+  );
 }
