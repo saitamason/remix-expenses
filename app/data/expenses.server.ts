e602ce61a19d44e8ff87d1f43f02c55ca4d8c2a3
dawidlehai <haidawidle@gmail.com>
2023-01-12 15:24:30 +0100

    Fetching expenses from DB

--- app/data/expenses.server.ts
+++ app/data/expenses.server.ts
@@ -6,11 +6,23 @@ export const addExpense = async (expenseData: {
   try {
     return await prisma.expense.create({
       data: {
-        title: expenseData.title,
+        title: String(expenseData.title),
         amount: +expenseData.amount,
         date: new Date(String(expenseData.date)),
       },
     });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getExpenses = async () => {
+  try {
+    const expenses = await prisma.expense.findMany({
+      orderBy: { date: "desc" },
+    });
+    return expenses;
   } catch (error) {
     console.log(error);
     throw error;
--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -1,23 +1,12 @@
-import { Link, Outlet } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 import { FaPlus, FaDownload } from "react-icons/fa";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "First expense",
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: "e2",
-    title: "Second expense",
-    amount: 19.99,
-    date: new Date().toISOString(),
-  },
-];
+import { getExpenses } from "~/data/expenses.server";
 
 export default function ExpensesLayout() {
+  const expenses = useLoaderData();
+
   return (
     <>
       <Outlet />
@@ -32,8 +21,10 @@ export default function ExpensesLayout() {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={DUMMY_EXPENSES} />
+        <ExpensesList expenses={expenses} />
       </main>
     </>
   );
 }
+
+export const loader: LoaderFunction = () => getExpenses();
