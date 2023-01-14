0e519a9cafe298eb01b29d8c639356f39f83cdec
dawidlehai <haidawidle@gmail.com>
2023-01-14 13:58:31 +0100

    Display information when no expenses have been found

--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -3,9 +3,11 @@ import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
 import { FaPlus, FaDownload } from "react-icons/fa";
 import { getExpenses } from "~/data/expenses.server";
+import type { Expense } from "~/types";
 
 export default function ExpensesLayout() {
-  const expenses = useLoaderData();
+  const expenses: Expense[] = useLoaderData();
+  const hasExpenses = expenses?.length > 0;
 
   return (
     <>
@@ -21,7 +23,15 @@ export default function ExpensesLayout() {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={expenses} />
+        {hasExpenses && <ExpensesList expenses={expenses} />}
+        {!hasExpenses && (
+          <section id="no-expenses">
+            <h1>No expenses found</h1>
+            <p>
+              Start <Link to="add">adding some</Link> today.
+            </p>
+          </section>
+        )}
       </main>
     </>
   );
